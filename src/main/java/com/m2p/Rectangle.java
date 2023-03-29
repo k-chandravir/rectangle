012e5b96cde26dacf22564a8bf67d28a1fadb786
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-29 15:50:59 +0530

    Req1: Add. Added Tests And The Function For The Area Of The Rectangle

--- src/main/java/com/m2p/Rectangle.java
+++ src/main/java/com/m2p/Rectangle.java
@@ -1,4 +1,12 @@
 package com.m2p;
 
+//class NegativeSideException extends Exception{
+//    public NegativeSideException(int anySideOfRectangle){
+//        super(anySideOfRectangle);
+//    }
+//}
 public class Rectangle {
+    public int area(int lengthOfRectangle, int breadthOfRectangle) {
+        return lengthOfRectangle * breadthOfRectangle;
+    }
 }
--- src/test/java/com/m2p/RectangleTest.java
+++ src/test/java/com/m2p/RectangleTest.java
@@ -1,8 +1,29 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RectangleTest {
-    Rectangle dummyRectanle = new Rectangle();
+    Rectangle dummyRectangle = new Rectangle();
+
+    @Nested
+    class Area{
+        @Test
+        void toReturnAreaAsEightWhenLengthIsFourAndBreadthIsTwo(){
+            int expectedArea = 8;
+
+            int actualArea = dummyRectangle.area(4, 2);
+
+            assertEquals(expectedArea, actualArea);
+        }
+
+//        @Test
+//        void toThrowExceptionWhenLengthOrBreadthIsNegative(){
+//            assertThrows()
+//        }
+    }
 
 }
